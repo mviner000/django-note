d691c221208e92887c265f7e7042a19f4d0450c4
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-28 11:37:17 +0800

    created_by and updated_by added to note database

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,9 @@ from django.utils import timezone
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_by = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+    updated_by = models.CharField(max_length=100, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
