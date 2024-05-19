a6d681e65adebab6dc0afab6fccb94666a158597
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-05-20 00:50:59 +0800

    backend for book borrow and return initially added

--- bookcart/models.py
+++ bookcart/models.py
@@ -6,6 +6,7 @@ class BookCart(models.Model):
     student = models.CharField(max_length=200, blank=True, null=True)
     books = models.ManyToManyField(Book, related_name='book_carts')
     is_borrowed_verified = models.BooleanField(default=False)
+    set_to_return = models.BooleanField(default=False)
     borrowed_verified_by = models.CharField(max_length=255, blank=True, null=True)
     is_returned_verified = models.BooleanField(default=False)
     returned_verified_by = models.CharField(max_length=255, blank=True, null=True)
--- bookcart/views.py
+++ bookcart/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.decorators import action
 from .models import BookCart
+from rest_framework.permissions import AllowAny
 from rest_framework import permissions 
 from .serializers import BookCartSerializer
 from rest_framework.response import Response
@@ -23,6 +24,18 @@ class BookCartViewSet(viewsets.ModelViewSet):
                 
         serializer.save()
         return Response(serializer.data)
+    
+    @action(methods=['patch'], detail=True, permission_classes=[AllowAny])
+    def verify_return(self, request, pk=None):
+        book_cart = self.get_object()
+        book_cart.is_returned_verified = True
+        book_cart.save()
+
+        for book in book_cart.books.all():
+            book.stock_quantity += 1
+            book.save()
+
+        return Response({'message': 'Return verification successful'})
 
 class BookCartListViewSet(viewsets.ModelViewSet):
     queryset = BookCart.objects.filter(is_borrowed_verified=False)  # Filter BookCarts by is_borrowed_verified
